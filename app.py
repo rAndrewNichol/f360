f4513771016c02bc30bf519e2265af3b5b8e31c9
Andrew Nichol <bigboiadmin@Andrews-MacBook-Pro.local>
2017-10-16 03:33:15 -0700

    trying

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from flask import Flask, jsonify, request, render_template, redirect
 gui = Flask(__name__)
 gui.config.from_object('config')
 
+# @gui.route('/')
+# def home():
+# 	return render_template('homepage.html',
+# 							title = "Home")
 @gui.route('/')
-def home():
-	return render_template('homepage.html',
-							title = "Home")
-@gui.route('/test')
+# @gui.route('/home')
 def test():
-	return render_template('test.html',title='ya')
+	return render_template('test.html')
+
+@gui.route('/<team>')
+def team1(team):
+	#keep params somehow?
+	return render_template(team + '.html')
 
 if __name__ == "__main__":
-	api.run()
+	gui.run()
