add275fefb6a0399278f97ebdde70441c0eedcf7
Andrew Nichol <bigboiadmin@Andrews-MacBook-Pro.local>
2017-10-28 18:51:51 -0700

    first final

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify, request, render_template, session, abort, redirect, url_for, Response
 import pymysql
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-#ADD TO HEROKU TODO
 from datetime import datetime
-from numpy import mean, std
+from numpy import std, arange, mean
 
 gui = Flask(__name__)
 gui.config.from_object('config')
@@ -97,8 +96,8 @@ def teams(team):
 		date = timestamp.split()[0].split('-')[1:]
 		hour = int(timestamp.split()[1].split(':')[0])
 		if hour <= 7:
-			date[1] = str(int(date[1]) - 1)
-			if date[1] == '0':
+			date[1] = "{0:0>2}".format(int(date[1]) - 1)
+			if date[1] == '00':
 				if date[0] == '11':
 					date = ['10','31']
 				elif date[0] == '12':
@@ -107,7 +106,10 @@ def teams(team):
 		return to_week[date]
 
 	def hundredize(rates):
-		return (sum(rates[:7]) * float(2.5), {"strong":rates[7],"improve":rates[8]})
+		if len(filter(None,rates)) == len(rates):
+			return (sum(rates[:7]) * float(2.5), {"strong":rates[7],"improve":rates[8]})
+		else:
+			return (None, None)
 
 	def process(data, people):
 		processed = {}
@@ -130,6 +132,8 @@ def teams(team):
 					#ignore it lol
 					continue
 				score, qual = hundredize(mem[1:])
+				if not score:
+					continue
 				if "others_rating" not in scores[mem_name]:
 					scores[mem_name]["others_rating"], scores[mem_name]["others_qual"] = (score,1), {"strong":[qual["strong"],],"improve":[qual["improve"],]}
 				else:
@@ -144,18 +148,6 @@ def teams(team):
 				return scores
 
 		return scores		
-
-	teams = {
-		1:['devina darmawan','kevin van der eijk','austin vuong','adam dada','tanvi mongia','aneesha lugani'],
-		2:['franklin rice','william sheng','fiona xie','dong eun suh','bryce king'],
-		3:['judith syau','andrew nichol','dana wu','christofe survian','palak thakur'],
-		4:['rhett gentile','ishan sharma','pouriya bagheri','pedro pablo correa hucke','manar safi'],
-		5:['gwynevere hunger','faraz kahen','keith sollers','varun agarwal','anthony blair'],
-		6:['siew hong ma','itzel romero','mingyang zhou','aayush patel','jenny liu'],
-		8:['neha burli','nitin manivasagan','monica kumaran', 'yoonji lu','aneesh chimbili'],
-		9:['sid iyer','alexander wing','anushree bhimani','arturo roman ordaz','bradford bruenell'],
-		10:['amy philip','samantha cristol','vanessa salas','kyle geitner','jonathan archer','henry keenan']
-	}
 
 	team = team.lower()
 
@@ -195,7 +187,16 @@ def teams(team):
 		scores[each_week] = process(sorted([line for line in weeks[each_week] if line[1].strip().lower() in people],reverse = True, 
 											key = lambda x:datetime.strptime(x[64], '%Y-%m-%d %H:%M:%S')),people)
 
-	names = [" ".join(name.split()[:-1]).title() for name in people]
+	names = []
+	for name in people:
+		if name == 'kevin van der eijk':
+			names.append("Kevin")
+		elif name == 'pedro pablo correa hucke':
+			names.append("Pablo")
+		else:
+			names.append(" ".join(name.split()[:-1]).title())
+
+
 	overall = {person:[0,0] for person in people}
 	for week in weeks_found:
 		for person in people:
@@ -233,6 +234,187 @@ def teams(team):
 @gui.route('/grades')
 @login_required
 def grades():
-	return render_template('grades.html')
+	to_week = {
+		"10-26":"Project","10-27":"Project","10-28":"Project","10-29":"Project","10-30":"Project",
+		"10-31":"Project","11-01":"Project",
+		"11-02":"10-31","11-03":"10-31","11-04":"10-31","11-05":"10-31","11-06":"10-31",
+		"11-07":"11-07","11-08":"11-07","11-09":"11-07","11-10":"11-07","11-11":"11-07","11-12":"11-07","11-13":"11-07",
+		"11-14":"11-14","11-15":"11-14","11-16":"11-14","11-17":"11-14","11-18":"11-14","11-19":"11-14","11-20":"11-14",
+		"11-21":"11-21","11-22":"11-21","11-23":"11-21","11-24":"11-21","11-25":"11-21","11-26":"11-21","11-27":"11-21",
+		# "11-28":"11-28","11-29":"11-28","11-30":"11-28","12-01":"11-28","12-02":"11-28","12-03":"11-28","12-04":"11-28",
+		# "12-05":"12-05","12-06":"12-05","12-07":"12-05","12-08":"12-05","12-09":"12-05","12-10":"12-05","12-11":"12-05",
+		# "12-12":"12-12","12-13":"12-12","12-14":"12-12","12-15":"12-12","12-16":"12-12","12-17":"12-12","12-18":"12-12"
+	}
+	teams = {
+		1:['devina darmawan','kevin van der eijk','austin vuong','adam dada','tanvi mongia','aneesha lugani'],
+		2:['franklin rice','william sheng','fiona xie','dong eun suh','bryce king'],
+		3:['judith syau','andrew nichol','dana wu','christofe survian','palak thakur'],
+		4:['rhett gentile','ishan sharma','pouriya bagheri','pedro pablo correa hucke','manar safi'],
+		5:['gwynevere hunger','faraz kahen','keith sollers','varun agarwal','anthony blair'],
+		6:['siew hong ma','itzel romero','mingyang zhou','aayush patel','jenny liu'],
+		8:['neha burli','nitin manivasagan','monica kumaran', 'yoonji lu','aneesh chimbili'],
+		9:['sid iyer','alexander wing','anushree bhimani','arturo roman ordaz','bradford bruenell'],
+		10:['amy philip','samantha cristol','vanessa salas','kyle geitner','jonathan archer','henry keenan']
+	}
+
+	def get_week(timestamp):
+		date = timestamp.split()[0].split('-')[1:]
+		hour = int(timestamp.split()[1].split(':')[0])
+		if hour <= 7:
+			date[1] = "{0:0>2}".format(int(date[1]) - 1)
+			if date[1] == '00':
+				if date[0] == '11':
+					date = ['10','31']
+				elif date[0] == '12':
+					date = ['11','30']
+		date = "-".join(date)
+		return to_week[date]
+
+	def hundredize(rates):
+		if len(filter(None,rates)) == len(rates):
+			return (sum(rates[:7]) * float(2.5), {"strong":rates[7],"improve":rates[8]})
+		else:
+			return (None, None)
+
+	def process(data, team):
+		people = teams[team]
+		processed = {}
+		persons_covered = set(people)
+		scores = {}
+		for person in persons_covered:
+			scores[person] = {}
+		for entry in data:
+			submitter = entry[1].strip().lower()
+			if submitter not in persons_covered:
+				continue
+			self_ratings = entry[5:14]
+			mems = [entry[14:24], entry[24:35], entry[34:44], entry[44:54], entry[54:64]]
+			scores[submitter]['scores_given'] = []
+			scores[submitter]["self_rating"], scores[submitter]["self_qual"] = hundredize(self_ratings)
+			for mem in mems:
+				if not mem[0]:
+					continue
+				mem_name = mem[0].strip().lower()
+				if mem_name not in people:
+					continue
+				score, qual = hundredize(mem[1:])
+				if not score:
+					continue
+				scores[submitter]['scores_given'].append(score)
+				if "others_rating" not in scores[mem_name]:
+					scores[mem_name]["others_rating"], scores[mem_name]["others_qual"] = (score,1), {"strong":[qual["strong"],],"improve":[qual["improve"],]}
+				else:
+					scores[mem_name]["others_rating"] = ((score + scores[mem_name]["others_rating"][0] * scores[mem_name]["others_rating"][1]) / 
+														(scores[mem_name]["others_rating"][1] + 1),
+															scores[mem_name]["others_rating"][1] + 1)
+					scores[mem_name]["others_qual"]["strong"].append(qual["strong"])
+					scores[mem_name]["others_qual"]["improve"].append(qual["improve"])
+
+			persons_covered.remove(submitter)
+			if not persons_covered:
+				return scores
+
+		for person in people:
+			if person in persons_covered:
+				scores[person]['missed'] = 1
+			else:
+				scores[person]['missed'] = 0
+
+		return scores	
+
+	db = pymysql.connect(host='us-cdbr-iron-east-05.cleardb.net',port='',user='bda0f11ccb424e',passwd='1e1bf253',db='heroku_55f2adb0c8d05ac')
+	cur = db.cursor()
+	all = """SELECT * FROM responses;"""
+	cur.execute(all)
+	data =  cur.fetchall() 
+	db.close()
+
+	weeks = {}
+	for line in data:
+		week = get_week(line[64])
+		if week in weeks:
+			weeks[week].append(line)
+		else:
+			weeks[week] = [line,]
+
+	all_weeks = ["Project","10-31","11-07","11-14","11-21"]
+	weeks_found = [week for week in all_weeks if week in weeks.keys()]
+
+	scores = {}
+	for team in teams:
+		scores[team] = {}
+		for week in weeks_found:
+			scores[team][week] = process(sorted([line for line in weeks[week] if line[1].strip().lower() in teams[team]],reverse = True, 
+													key = lambda x:datetime.strptime(x[64], '%Y-%m-%d %H:%M:%S')),team)
+
+	# print scores
+	overall = {}
+	for team in teams:
+		for person in teams[team]:
+			overall[person] = [0,0,0,[]]
+		for week in scores[team]:
+			for person in scores[team][week]:
+				if 'others_rating' in scores[team][week][person]:
+					overall[person][0] += scores[team][week][person]['others_rating'][0] * scores[team][week][person]['others_rating'][1]
+					overall[person][1] += scores[team][week][person]['others_rating'][1]
+				if 'missed' in scores[team][week][person]:
+					overall[person][2] += scores[team][week][person]['missed']
+				if 'scores_given' in scores[team][week][person]:
+					overall[person][3].extend(scores[team][week][person]['scores_given']) 
+
+	# print overall
+
+
+	team_avgs = []
+	for team in teams:
+		team_sum = 0
+		for person in teams[team]:
+			if overall[person][3]:
+				overall[person][3] = std(overall[person][3])
+			else:
+				overall[person][3] = 0
+			if overall[person][1]:
+				team_sum += overall[person][0] / float(overall[person][1])
+				overall[person][1] = round(overall[person][0] / float(overall[person][1]),1)
+		team_avgs.append(team_sum/float(len(teams[team])))
+
+	team_sd = std(team_avgs)
+	team_mean = mean(team_avgs)
+	team_zs = [(each - team_mean)/team_sd for each in team_avgs]
+	each_team_scale_factor = []
+	for i in xrange(len(team_avgs)):
+		if team_avgs[i] != 0:
+			each_team_scale_factor.append((team_mean+(8*team_zs[i]))/team_avgs[i])
+		else:
+			each_team_scale_factor.append(1.0)
+
+	for i in xrange(len(teams)):
+		for person in teams[teams.keys()[i]]:
+			overall[person][0] = overall[person][1] * each_team_scale_factor[i]
+
+
+	class_avg = mean([team_mean + (8*z) for z in team_zs])
+	class_sd = std([value[0] for (key,value) in overall.iteritems()])
+
+
+	for person in overall:
+		overall[person][0] = 85 + (8 * ( (overall[person][0] - class_avg )/class_sd ))
+
+	sd_adjustments = [round(step - 4.6,1) for step in arange(0,9.4,.2)]
+
+	sorted_by_sd = sorted(overall.keys(), key = lambda x: overall[x][3])
+	for i in xrange(len(sorted_by_sd)):
+		try:
+			overall[sorted_by_sd[i]][0] += sd_adjustments[i]
+		except:
+			pass
+		overall[sorted_by_sd[i]][0] -= 5 * overall[sorted_by_sd[i]][2]
+		if overall[sorted_by_sd[i]][0] >= 100:
+			overall[sorted_by_sd[i]][0] = 100
+		overall[sorted_by_sd[i]][0] = round(overall[sorted_by_sd[i]][0],1)
+
+	# print overall
+	return render_template('grades.html', overall = overall)
+
 if __name__ == "__main__":
 	gui.run()
